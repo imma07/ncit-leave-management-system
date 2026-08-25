92be7b407c15535c85da9add8d6b7434d3f06bd9
imma <ismail.mohamed@ncit.gov.mv>
2026-08-25 14:24:46 +0500

    primevue set - this is the component library that will be used

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import ToastService from 'primevue/toastservice';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,7 +22,16 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el);
+            .use(ToastService)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura,
+                    options: {
+                        darkModeSelector: 'light',
+                    }
+                },
+                license: 'eyJpZCI6IjM5MDBkZDlhLWIwNTItNDIxNi1iZGQ3LTM5NzIwNzFkZDEwYiIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODc1OTQ1MzIsImV4cCI6MTgxOTEzMDUzMn0.I23dzHDDStQ8GodY8Ee1GRKolBiO__BIyFQHTcnfKj-Sj5fHaOFS2ZK64r2Gg-JySTlGsofXG8jSvyEI_L0JCA'
+            }).mount(el);
     },
     progress: {
         color: '#4B5563',
